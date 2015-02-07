eb3e11389cffb8b201cad997501800e334e06c55
Lunan <Simon@Kattler.dk>
2015-02-07 05:52:21 +0100

    Jumping "virker"
    Du kan hoppe, men i luften har du 1 sek lift, som ikke nødvendigvis skal
    bruges med det samme.

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -1,50 +1,50 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Movement : MonoBehaviour {
 	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
-    public bool isGrounded = true;
+	public float jumpSpeed = 10.0F;
+	public float gravity = 5.0F;
+    public bool isGrounded = false;
     private bool up = false;
 	private float jumptime = 0F;
-
 
 	private Vector3 moveDirection = Vector3.zero;
 
 
 	void Update() {
-		Debug.LogError ("Tester");
 		Debug.Log(jumptime);
-		if (jumptime < 1F && !isGrounded) {
+		Debug.Log ("move: "+moveDirection.y);
+		if (!up)
+			Debug.Log ("up is false");
+
+        if (moveDirection.y > 0.5F) {
+			isGrounded = false;
+			up = true;
+		} else if (moveDirection.y < -0.5F) {
+			up = false;
+			isGrounded = false;
+		} else if (up == false) {
+			isGrounded = true;
+		}
+		if (jumptime < 1F) {
 			moveDirection.y = jumpSpeed;
 			jumptime += Time.deltaTime;
-		} else
-			jumptime = 0F;
-
-        if (moveDirection.y > 0.5F)
-        {
-            isGrounded = false;
-			up = true;
-        }
-        else if (moveDirection.y < -0.5F)
-        {
-            up = false;
-            isGrounded = false;
-        }
-        else if(up == false)
-            isGrounded = true;
+		}
 		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 		moveDirection = transform.TransformDirection(moveDirection);
 		moveDirection *= speed;
-		if (isGrounded) {
+		//if (isGrounded) {
 
             if (Input.GetButton("Jump")) {
                 moveDirection.y = jumpSpeed;
-				jumptime += Time.deltaTime;
+				jumptime = Time.deltaTime;
+				Debug.Log("Hopper");
             }
+		//}
+		if (!(jumptime < 1F)) {
+			moveDirection.y -= gravity * Time.deltaTime;
 		}
-		moveDirection.y -= gravity * Time.deltaTime;
 		transform.Translate(moveDirection * Time.deltaTime);
 	}
 }
